2bc04007c1e7ec48135b087300df6f2dc0d854f7
Héjjas Attila <attila.h9@gmail.com>
2016-06-26 15:53:03 +0200

    add a shuffle method

--- web/js/utils.js
+++ web/js/utils.js
@@ -90,3 +90,29 @@ function isEquivalent(a, b) {
     // are considered equivalent
     return true;
 }
+
+/**
+ * Fisher-Yates
+ * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * 
+ * @param array array
+ * @returns array
+ */
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
