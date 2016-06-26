852aada7856c94694c37a2e1281e4e5f85bebdcf
Morár Anna <anna.arach@gmail.com>
2016-06-26 16:37:04 +0200

    random kártyák

--- web/js/index.js
+++ web/js/index.js
@@ -19,6 +19,19 @@ var uniqueCards = [
 var createCards = function(data){
     var mergedCards = data.concat(data);
     console.log(mergedCards);
+
+    shuffle(mergedCards);
+    console.log(mergedCards);
+
+    var cardTemplate = '<div class="card {color}">{index}</div>';
+
+    var $field = $('#field');
+    for (var i=0; i<mergedCards.length; i++) {
+        var cardHtml = cardTemplate;
+        cardHtml = cardHtml.replace('{color}', mergedCards[i]);
+        cardHtml = cardHtml.replace('{index}', i.toString());
+        $field.append(cardHtml);
+    }
 };
 
 //function create() {
